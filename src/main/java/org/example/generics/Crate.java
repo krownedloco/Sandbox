f5fef1a0546c45dacc013650eecde31bab239383
krownedloco <frankconchos88@gmail.com>
2022-09-22 19:51:32 -0700

    Generics

---
+++ src/main/java/org/example/generics/Crate.java
@@ -0,0 +1,29 @@
+package org.example.generics;
+
+import java.util.Objects;
+
+public class Crate<T extends Box<?>> {
+
+    T t;
+
+    public Crate(T t) {
+        this.t = t;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public <B extends Box<?>> Crate<Box<?>> clone(B b){
+        Crate<Box<?>> newCrate = new Crate<>(b);
+        return newCrate;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Crate{");
+        sb.append("t=").append(t);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/example/generics/GenericUsage.java
@@ -0,0 +1,24 @@
+package org.example.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericUsage {
+
+    public static void main(String[] args) {
+        Box<Integer> integers = new Box<>(1);
+        Box<Double> doubles = new Box<>(1.0);
+
+        List<Box<?>> numbers = new ArrayList<>();
+        numbers.add(doubles);
+        numbers.add(integers);
+
+        Crate<Box<?>> crate = new Crate(integers);
+        System.out.println(crate.getT());
+
+        Crate<Box<Double>> crate2 = new Crate(doubles);
+
+
+
+    }
+}
